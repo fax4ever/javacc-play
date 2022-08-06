fe0c544c55a738fde37f55f2dce37cea4d5f3426
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-08-06 23:17:18 +0200

    Add more test

--- javacc-proto-play/src/test/java/fax/play/ProtoParserTest.java
+++ javacc-proto-play/src/test/java/fax/play/ProtoParserTest.java
@@ -4,17 +4,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.InputStream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ProtoParserTest {
 
-   @Test
-   public void test() throws Exception {
-      try (InputStream is = this.getClass().getClassLoader().getResourceAsStream("proto/simple.proto")) {
+   @ParameterizedTest
+   @ValueSource(strings = {"proto2", "proto3"})
+   public void proto3(String syntaxType) throws Exception {
+      try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(syntaxType + "/simple.proto")) {
          ProtoParser parser = new ProtoParser(is);
          ASTInput input = parser.Input();
+         assertThat(input.jjtGetNumChildren()).isEqualTo(2);
 
-         assertThat(input).isNotNull();
+         Node syntax = input.jjtGetChild(0);
+         assertThat(syntax).isInstanceOf(ASTSyntaxPart.class);
+         assertThat(syntax.jjtGetNumChildren()).isEqualTo(1);
+
+         Node syntaxValue = syntax.jjtGetChild(0);
+         assertThat(syntaxValue).isInstanceOf(ASTCStringPart.class);
+         assertThat(((SimpleNode) syntaxValue).jjtGetValue().toString()).isEqualTo("\"" + syntaxType + "\"");
+
+         Node message = input.jjtGetChild(1);
+         assertThat(message).isInstanceOf(ASTMessagePart.class);
       }
    }
 }
